abfaf6234c954b77f1127ea5674d8a1910064378
Abdelrahman Shaban <158591383+AbdelrahmanShabanh@users.noreply.github.com>
2025-07-10 00:36:25 +0300

    Main.java

--- Fawry-Internship-Challenge-main/Fawry-Internship-Challenge-main/src/com/fawry/challenge/Main.java
+++ Fawry-Internship-Challenge-main/Fawry-Internship-Challenge-main/src/com/fawry/challenge/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static ShoppingCart shoppingCart = new ShoppingCart();
       
     public static Customer customer = new Customer("abdelrahman", "portsaid", "0888", 20000.0);
-    public static Customer customer = new Customer("Haneen ", "cairo", "0777", 20000.0);
+  
     public static void main(String[] args) {
         createShoppingCartData();
         CheckoutOrder checkoutOrder = new CheckoutOrder(customer, shoppingCart);
